d0b18dbc780c770b51020c0c17af29d41d5cb83d
mplacona <marcos.placona@gmail.com>
2015-07-22 17:48:40 +0100

    Add Friend model

---
+++ BusyFriend/Models/Friend.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusyFriend.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
